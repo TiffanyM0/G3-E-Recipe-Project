e438114f6fe9cb3e4b29f3d89723fd0a2e9ab32f
peter <pngugi.wainaina@gmail.com>
2023-12-14 22:57:25 +0300

    post request to the server completed

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -9,3 +9,19 @@ function Reviews({ onSubmitFeedback }) {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    // Make a POST request to the server to save the feedback
+    axios
+      .post("http://localhost:3000/feedbacks", { text: feedbackText })
+      .then((response) => {
+        console.log("Feedback successfully posted:", response.data);
+        setFeedbackText("");
+        // Call the parent component's function to handle the feedback submission
+        onSubmitFeedback(response.data);
+        // Display thank-you message as an alert
+        alert("Thank you for submitting your feedback!");
+      })
+      .catch((error) => {
+        console.error("Error posting feedback:", error);
+      });
+  };
