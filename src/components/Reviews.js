44ecc4f4154911d46575e2d62bf7b2e6f326c07c
peter <pngugi.wainaina@gmail.com>
2023-12-14 22:53:23 +0300

    function reviews done

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,3 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+function Reviews({ onSubmitFeedback }) {
+  const [feedbackText, setFeedbackText] = useState();
+
+  const handleFeedbackChange = (event) => {
+    setFeedbackText(event.target.value);
+  };
