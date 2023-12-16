cd3238db71e7f064e37ca84a13bce733ea004e67
Kevin <kevin.kiseli@student.moringaschool.com>
2023-12-16 20:16:40 +0300

    Introducing the AboutUs and ContactUs component

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const ContactUs = () => {
     return (
-        <div className = "contact-us">
+        <div className="contact-us">
+            <h2>Contact Us</h2>
+            <p>
+                
+            </p>
 
         </div>
     )
