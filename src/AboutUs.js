9c61d49b24c80ac3776977b283167b26c2392db2
Kevin <kevin.kiseli@student.moringaschool.com>
2023-12-16 20:08:16 +0300

    Update on About Us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -4,7 +4,11 @@ import React from "react";
 const AboutUs = () => {
   return (
     <div className="about-us">
-     
+      <h2>About Us</h2>
+      <p>
+        Welcome to our virtual kitchen E-Recipe, where the joy of cooking meets
+        the essence of deliciousness.
+      </p>
     </div>
   );
 };
