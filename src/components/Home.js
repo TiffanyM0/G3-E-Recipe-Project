b2099d519f55062a4af13e319b068a28953a01ec
peter <pngugi.wainaina@gmail.com>
2023-12-17 13:04:51 +0300

    function home done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,3 +3,17 @@ import SearchBar from "./SearchBar";
 import Category from "./Category";
 import FilterMeal from "./FilterMeal";
 import SelectMeal from "./SelectMeal";
+
+
+function Home({
+    searchedMeal,
+    setSearchedMeal,
+    setSelectedCategory,
+    categories,
+    meals,
+    handleMealClick,
+    selectedMeal,
+    handleCloseDetails,
+    setMeal,
+    instructionsRef,
+  }) 
